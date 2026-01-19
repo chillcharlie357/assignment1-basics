05fdf829958f693fdc94c8ffc94331b4d43dfc7e
heleyang <heleyang0311@163.com>
2026-01-19 23:12:59 +0800

    rope cached

--- cs336_basics/transformer/rope.py
+++ cs336_basics/transformer/rope.py
@@ -1,3 +1,4 @@
+from turtle import forward
 from torch import nn
 import torch
 from .utils import get_device
@@ -20,18 +21,28 @@ class RotaryPositionalEmbedding(nn.Module):
 
         self.d_k = d_k
         self.max_seq_len = max_seq_len
-        
+
+        # 计算sin, cos cache
+        self._precompute_sin_cos()
         
 
-    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
-        """
-        x: (..., seq_len, d_k)
-        token_positions: (..., seq_len), specifying the token positions of x along the sequence dimension.
-        """
-        x = x.to(self.device)
-        token_positions = token_positions.to(self.device)
         
+    def _precompute_sin_cos(self):
+        all_positions =  torch.arange(self.max_seq_len).float().to(self.device)
+        all_Theta = einsum(
+            all_positions, self.theta,
+            "max_seq_len, d_k_half -> max_seq_len d_k_half"
+        )
+        all_sin = torch.sin(all_Theta).to(self.device)
+        all_cos = torch.cos(all_Theta).to(self.device)
+        all_sin = repeat(all_sin, "max_seq_len d_k_half -> max_seq_len (d_k_half two)", two=2)
+        all_cos = repeat(all_cos, "max_seq_len d_k_half -> max_seq_len (d_k_half two)", two=2)
 
+        self.register_buffer("sin", all_sin, persistent=False)
+        self.register_buffer("cos", all_cos, persistent=False)
+
+
+    def _no_cache(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
         Theta = einsum(
             token_positions.float(), self.theta,
             "seq_len, d_k_half -> seq_len d_k_half"
@@ -49,6 +60,36 @@ class RotaryPositionalEmbedding(nn.Module):
         x_even = x[..., 0::2] # 0, 2, 4, ...
         x_odd = x[..., 1::2] # 1, 3, 5, ...
         half_neg_x = torch.stack((-x_odd, x_even), dim=-1)
+        # 展开为[-x_1, x_0, -x_3, x_2, ...]
         half_neg_x = rearrange(half_neg_x, "... d_k_half two -> ... (d_k_half two)")
         
         return x * cos +  half_neg_x * sin
+    
+    def _cached(self,x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        ids = token_positions.long()
+
+        sin_slice = self.sin[ids].to(self.device)
+        cos_slice = self.cos[ids].to(self.device)
+
+        x_even = x[..., 0::2] # 0, 2, 4, ...
+        x_odd = x[..., 1::2] # 1, 3, 5, ...
+        half_neg_x = torch.stack((-x_odd, x_even), dim=-1)
+        # 展开为[-x_1, x_0, -x_3, x_2, ...]
+        half_neg_x = rearrange(half_neg_x, "... d_k_half two -> ... (d_k_half two)")
+
+
+        return x * cos_slice + half_neg_x * sin_slice
+
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        """
+        x: (..., seq_len, d_k)
+        token_positions: (..., seq_len), specifying the token positions of x along the sequence dimension.
+        """
+        x = x.to(self.device)
+        token_positions = token_positions.to(self.device)
+
+        # return self._no_cache(x, token_positions)
+        return self._cached(x, token_positions)
+        
+        
