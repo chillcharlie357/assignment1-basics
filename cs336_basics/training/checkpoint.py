00c8ffb439ca10a26141bac250aaa949bd45212a
heleyang <heleyang0311@163.com>
2026-01-25 17:25:01 +0800

    checkpoint

---
+++ cs336_basics/training/checkpoint.py
@@ -0,0 +1,23 @@
+import typing
+from torch import nn, Tensor, optim
+import os
+
+import torch
+def save_checkpoint(model: nn.Module, optimizer: optim.Optimizer, iteration: int, out : str | os.PathLike | typing.BinaryIO | typing.IO[bytes]):
+    states = {
+        "iter": iteration,
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+
+    torch.save(states, out)
+
+
+def load_checkpoint(src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes], model: nn.Module, optimizer: optim.Optimizer) -> int:
+    states = torch.load(src)
+
+    iter = int(states['iter'])
+    model.load_state_dict(states['model'])
+    optimizer.load_state_dict(states['optimizer'])
+
+    return iter
